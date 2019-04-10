80c88cd33aa6aaf51ec05b6e079c5cf8f316d619
luisbeltran1982 <47642586+luisbeltran1982@users.noreply.github.com>
2019-04-10 09:11:49 -0400

    Update 100-install-core-software-xfce.sh

--- 100-install-core-software-xfce.sh
+++ 100-install-core-software-xfce.sh
@@ -33,19 +33,39 @@ echo "################################################################"
 echo "###############       mirrorlist updated      ###################"
 echo "################################################################"
 
+###############################################################################################
+
 #software from 'normal' repositories
-sudo pacman -S --noconfirm --needed git ntfs-3g wget curl socat
-sudo pacman -S --noconfirm --needed mpv youtube-dl mps-youtube
-sudo pacman -S --noconfirm --needed neofetch numlockx
-sudo pacman -S --noconfirm --needed noto-fonts ttf-roboto gtk-engine-murrine
-sudo pacman -S --noconfirm --needed chromium qbittorrent mcomix libreoffice-still pepper-flash
+sudo pacman -S --noconfirm --needed ntfs-3g wget curl
+sudo pacman -S --noconfirm --needed mpv youtube-dl mps-youtube socat gtk-engine-murrine
+sudo pacman -S --noconfirm --needed neofetch numlockx geany easytag kodi
+sudo pacman -S --noconfirm --needed chromium pepper-flash libreoffice-still firefox
 
 ###############################################################################################
 
 # installation of zippers and unzippers
-sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract p7zip lrzip file-roller
+sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj p7zip lrzip file-roller
 
 ###############################################################################################
+
+echo "Installing fonts from Arch Linux repo"
+
+sudo pacman -S adobe-source-sans-pro-fonts --noconfirm --needed
+sudo pacman -S noto-fonts --noconfirm --needed
+sudo pacman -S terminus-font --noconfirm --needed
+sudo pacman -S ttf-bitstream-vera --noconfirm --needed
+sudo pacman -S ttf-dejavu --noconfirm --needed
+sudo pacman -S ttf-droid --noconfirm --needed
+sudo pacman -S ttf-roboto --noconfirm --needed
+sudo pacman -S ttf-ubuntu-font-family --noconfirm --needed
+sudo pacman -S ttf-hack --noconfirm --needed
+sudo pacman -S ttf-inconsolata --noconfirm --needed
+
+
+echo "################################################################"
+echo "####        Fonts from Arch Linux repo have been installed        ####"
+echo "################################################################"
+
 
 ########################################
 ########          YAY	        ########
@@ -79,7 +99,7 @@ else
 
 	# Just checking if installation was successful
 	if pacman -Qi $package &> /dev/null; then
-	
+
 	echo "################################################################"
 	echo "#########  "$package" has been installed"
 	echo "################################################################"
@@ -95,9 +115,8 @@ else
 fi
 
 #Software from AUR
-yay -S --noconfirm ttf-roboto-mono droid-min gotop-bin xfce-theme-greybird-git elementary-xfce-icons-git tiny-media-manager-bin pyradio-git dropbox
+yay -S --noconfirm --needed gotop-bin pyradio-git mpdscribble mpd-notification xfce-theme-greybird elementary-xfce-icons-git tiny-media-manager dropbox
 
 echo "################################################################"
 echo "###################    core software installed  ################"
 echo "################################################################"
-
